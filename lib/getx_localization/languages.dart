f37b2adf9b468a6aeaddf6b1b6760d3180c6ccc8
SHOAIBAHMED12842 <46914029+SHOAIBAHMED12842@users.noreply.github.com>
2023-03-14 16:07:41 +0500

    Update languages.dart

--- lib/getx_localization/languages.dart
+++ lib/getx_localization/languages.dart
@@ -2,15 +2,15 @@ import 'package:get/get_navigation/src/root/internacionalization.dart';
 
 class Languages extends Translations {
 
-  @override
   Map<String, Map<String, String>> get keys => {
         'ur_PK': {
           'message': 'آپ کا نام کیا ہے؟',
-          'name': 'آصف تاج',
+          'name': 'شعیب احمد'
         },
         'en_US': {
           'message': 'My name is?',
-          'name': 'Asif Taj',
+          'name': 'Shoaib Ahmed',
         },
       };
+ 
 }
